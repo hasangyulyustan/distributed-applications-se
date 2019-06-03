248096c10f21c11ed04661bf5e4c42fb92c1d2c0
Hasan Gyulyustan <hasan_17@abv.bg>
2019-06-03 15:28:23 +0100

    Exercise 5 added.

---
+++ exercises/05/MovieCatalog/MC.ApplicationServices/DTOs/GenreDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MC.ApplicationServices.DTOs
+{
+    public class GenreDto : IValidate
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public bool Validate()
+        {
+            return !String.IsNullOrEmpty(Name);
+        }
+    }
+}
